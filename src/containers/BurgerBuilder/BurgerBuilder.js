5a907cec7d81b0b39e3d3bc99ce2a231a132e802
Nicolas Deschamps <ndeschamps@sparkhire.com>
2020-06-12 11:43:14 -0500

    sending orders using post requests

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from '../../axios-orders';
 
 import Aux from "../../hoc/Aux/Aux";
 import Burger from '../../components/Burger/Burger';
@@ -86,7 +87,27 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue!');
+        // alert('You continue!');
+
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice.toFixed(2),
+            customer: {
+                name: 'John Smith',
+                address: {
+                    street: '1 Test Street',
+                    zipCode: '12345',
+                    country: 'United-States'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'fasstest'
+        }
+
+        // .json added for Firebase to work correctly
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     render() {
